2557fb696f7f783904499c475a78c891d405b15a
Кирилл Зайцев <kirill@Kirills-MacBook-Air.local>
2024-03-01 03:10:05 +0500

    hotfix: fixed text in work example

--- src/components/UI/shared/Logo/Logo.tsx
+++ src/components/UI/shared/Logo/Logo.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const Logo = () => {
   return (
     <Link href={'/'} className={m.container}> 
-      <Image className={m.img} src={LogoImg} alt="" />
+      <Image className={m.img} src={LogoImg} priority={true} alt="" />
       <div className={m.textWrapper}>
           <h1 className={m.logoTitle}>Omni Vitrum</h1>
           <label className={m.logoLable}>Производство и продажа</label>
