f00da73ceb5b7708b6410209bf2f538da4504b4e
Кирилл Зайцев <kirill@Kirills-MacBook-Air.local>
2024-02-29 23:55:25 +0600

    hotfix: fixed calc logic in calculator

--- src/components/UI/Calculate/helper/calc.helper.ts
+++ src/components/UI/Calculate/helper/calc.helper.ts
@@ -1,52 +1,47 @@
 import { useState } from "react";
 
+// Основа
 const useCalc = () => {
   const pi = 3.1415926535;
-  const [corner, setCorner] = useState<number>(0);
-  const [midline, setMidline] = useState<number>(0);
+  const [corner, setCorner] = useState(0);
+  const [midline, setMidline] = useState(0);
 
   const handleCalculate = ({ firstParams, secondParams }: any) => {
-    let radius = 0, chord = 0, deflection = 0;
+    let radius = 0, chord = 0, deflection = 0, thickness = 0, type = "Внутренний";
 
-    // Определение значений на основе входных параметров
     if (firstParams.param1.toLowerCase() === "радиус") {
       radius = parseFloat(firstParams.value1);
-    } else if (firstParams.param1.toLowerCase() === "прогиб") {
-      deflection = parseFloat(firstParams.value1);
+    } else if (firstParams.param1.toLowerCase() === "хорда") {
+      chord = parseFloat(firstParams.value1);
     }
 
-    if (secondParams.param2?.toLowerCase() === "прогиб") {
-      deflection = parseFloat(secondParams.value2 || '0');
-    } else if (secondParams.param2?.toLowerCase() === "хорда") {
-      chord = parseFloat(secondParams.value2 || '0');
+    if (secondParams.param2?.toLowerCase() === "хорда") {
+      chord = parseFloat(secondParams.value2);
+    } else if (secondParams.param2?.toLowerCase() === "радиус") {
+      radius = parseFloat(secondParams.value2);
     }
 
-    // Вычисления
-    if (chord === 0 && radius !== 0 && deflection !== 0) {
-      chord = Math.sqrt((8 * radius * deflection) - (4 * deflection * deflection));
-    }
-
-    if (radius === 0 && chord !== 0 && deflection !== 0) {
-      radius = (deflection / 2) + (chord * chord) / (8 * deflection);
-    }
-
-    const newCorner = radius !== 0 ? ((2 * Math.asin(chord / (radius * 2))) * 180) / pi : 0;
+    const newCorner = radius > 0 ? (2 * Math.asin(chord / (2 * radius)) * 180) / pi : 0;
     setCorner(newCorner);
 
-    const thickness = secondParams.mm || 0;
-    const type = secondParams.type || "Внутренний";
+    if (secondParams.param3) {
+      type = secondParams.param3;
+    }
+    if (secondParams.param4) {
+      thickness = parseFloat(secondParams.param4);
+    }
 
     let newMidline = 0;
     if (type.toLowerCase() === "внутренний") {
       newMidline = (radius + (thickness / 2)) * 2 * pi * newCorner / 360;
-    } else {
+    } else if (type.toLowerCase() === "наружный") {
       newMidline = (radius - (thickness / 2)) * 2 * pi * newCorner / 360;
     }
 
-    setMidline(newMidline / 2);
+    setMidline(newMidline);
   };
 
   return { corner, midline, handleCalculate };
-}
+};
 
 export { useCalc };
